f5579c671279be3d2c7d19bb0a16ac911793139b
LameezRushin <162303399+LameezRushin@users.noreply.github.com>
2024-12-22 22:35:32 +0200

    Add Footer component

---
+++ src/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      Coded by{' '}
+      <a
+        href="https://github.com/LameezRushin"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Lameez Rushin
+      </a>
+      , is open-sourced on{' '}
+      <a
+        href="https://github.com/LameezRushin/lameez-weather-project"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Github
+      </a>
+      , and hosted by{' '}
+      <a
+        href="https://lameez-weather-app-react.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Netlify
+      </a>
+      .
+    </footer>
+  );
+}
+
+export default Footer;
